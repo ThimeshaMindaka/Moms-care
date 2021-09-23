e1c5eb17b2cf12ec647f0c1b007b73b3bcdb2bff
KasunShyamal <kshyamal48@gmail.com>
2021-09-23 16:12:32 +0530

    cal done

--- app/src/main/java/com/example/momscare/Medical/Medical_info.java
+++ app/src/main/java/com/example/momscare/Medical/Medical_info.java
@@ -2,13 +2,32 @@ package com.example.momscare.Medical;
 
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.momscare.R;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Medical_info extends AppCompatActivity {
+    // ceating references
+
+    Button btn14, btn16, btn17;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_medical_information);
+
+        btn14 = findViewById(R.id.btn14);
+        btn16 = findViewById(R.id.btn16);
+        btn17 = findViewById(R.id.btn17);
     }
+
+    //navigation
+    public void openduedate_cal(View view) {
+        Intent opnDue = new Intent( Medical_info.this,Duedate_cal.class);
+        startActivity(opnDue);
+    }
+
 }
